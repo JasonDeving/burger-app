477719ff4fb566b263c165f9af01cf26df29b91b
jasondeving <aznj50@hotmail.com>
2016-10-05 18:15:05 -0700

    push to github

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers(burger_name) VALUES ('Get LIT Burger');
+INSERT INTO burgers(burger_name) VALUES ('The Weekend Rager Burger');
+INSERT INTO burgers(burger_name) VALUES ('Pastee Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Crochet Hamburger');
